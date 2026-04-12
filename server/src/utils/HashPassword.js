19b6b8278f08fa97dba48d6ada9e491fb130cc43
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-12 13:55:26 +0800

    feat(utils): add password hashing and comparison utility

---
+++ server/src/utils/HashPassword.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+async function hashPassword(password) {
+  return await bcrypt.hash(password, saltRounds);
+}
+
+async function comparePassword(password, hashedPassword) {
+  return await bcrypt.compare(password, hashedPassword);
+}
+
+export { hashPassword, comparePassword };
