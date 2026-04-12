9c8504877137a9dce09fa6b9187f4a7a3f777b2a
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-12 17:38:29 +0800

    feat(models): add Message model

---
+++ server/src/models/MessageModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const messageModel = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Message", messageModel);
