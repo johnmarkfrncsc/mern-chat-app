2ee687d64a2280fb729933015e51059f91eb1773
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-12 16:12:28 +0800

    feat(controllers): add register and login controllers

---
+++ server/src/controllers/LoginController.js
@@ -0,0 +1,20 @@
+import loginUser from "../services/AuthLoginUser.js";
+
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { user, token } = await loginUser(email, password);
+    res.status(200).json({
+      data: user,
+      token,
+      message: "Log in successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      data: null,
+      message: error.message,
+    });
+  }
+};
+
+export default loginController;
---
+++ server/src/controllers/RegisterController.js
@@ -0,0 +1,19 @@
+import registerUser from "../services/AuthRegisterUser.js";
+
+const registerController = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = await registerUser(username, email, password);
+    return res.status(201).json({
+      data: newUser,
+      message: `Registered successfully ${username}`,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      data: null,
+      message: error.message,
+    });
+  }
+};
+
+export default registerController;
