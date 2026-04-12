c89bc859485afda760a5fd3c1a05a16d580a20fc
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-12 16:35:37 +0800

    test(auth): verify register and login endpoints working

--- server/src/routes/AuthRoutes.js
+++ server/src/routes/AuthRoutes.js
@@ -8,3 +8,34 @@ router.post("/register", registerController);
 router.post("/login", loginController);
 
 export default router;
+
+// register:
+//json
+// {
+//     "data": {
+//         "username": "testuser",
+//         "email": "test@gmail.com",
+//         "password": "$2b$10$MQaO7w/WqF0RXoEheLn9T.OtWwiXN0mm5Zc.KQpEaTg4JRYrnVxdK",
+//         "_id": "69db584cfbfa27764d91dd96",
+//         "createdAt": "2026-04-12T08:31:08.049Z",
+//         "updatedAt": "2026-04-12T08:31:08.049Z",
+//         "__v": 0
+//     },
+//     "message": "Registered successfully testuser"
+// }
+
+//login:
+// json
+// {
+//     "data": {
+//         "_id": "69db584cfbfa27764d91dd96",
+//         "username": "testuser",
+//         "email": "test@gmail.com",
+//         "password": "$2b$10$MQaO7w/WqF0RXoEheLn9T.OtWwiXN0mm5Zc.KQpEaTg4JRYrnVxdK",
+//         "createdAt": "2026-04-12T08:31:08.049Z",
+//         "updatedAt": "2026-04-12T08:31:08.049Z",
+//         "__v": 0
+//     },
+//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiNjlkYjU4NGNmYmZhMjc3NjRkOTFkZDk2IiwiaWF0IjoxNzc1OTgyNzMwLCJleHAiOjE3NzYwNjkxMzB9.0eXIC2gu2bs5rmUtq4713NiH5FnhH7oiosJ1Sy0mYY4",
+//     "message": "Log in successful"
+// }
