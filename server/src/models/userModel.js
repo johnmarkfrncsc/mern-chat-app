ad60d7b1c0d84a7d66f9d6adc667a04d2a7f699b
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-12 13:31:36 +0800

    feat(models): create initial user schema

---
+++ server/src/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserModel = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.Schema("User", UserModel);
