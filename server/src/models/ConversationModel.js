8c7cbed73800d0934aee8d51b74d1ce1a71080f1
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-12 17:30:36 +0800

    feat(models): add Conversation model

---
+++ server/src/models/ConversationModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const convoModel = new mongoose.Schema(
+  {
+    participants: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      required: true,
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Conversation", convoModel);
