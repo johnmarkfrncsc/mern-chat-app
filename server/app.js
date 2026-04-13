7c42dc02e45efba558bfed00bf57d26f57f577ef
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-13 17:10:30 +0800

    refactor(app): remove database and server startup from app.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import connectMONGODB from "./src/config/db.js";
 import AuthRoutes from "./src/routes/AuthRoutes.js";
 import MessageRoutes from "./src/routes/MessageRoutes.js";
-
-// declare dotenv config
-dotenv.config();
 
 const app = express();
 
@@ -17,18 +12,5 @@ app.use(express.json());
 //route
 app.use("/api/auth", AuthRoutes);
 app.use("/api/message", MessageRoutes);
-
-//connect mongodb then start the server
-connectMONGODB()
-  .then(() => {
-    const PORT = process.env.PORT || 8080;
-    app.listen(PORT, () => {
-      console.log(`Server running to port http:localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Error in connecting in mongodb", err);
-    process.exit(1);
-  });
 
 export default app;
