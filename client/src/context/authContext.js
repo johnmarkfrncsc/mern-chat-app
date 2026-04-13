5adb1d5305667e904d5bf402ac64de0be7541f1c
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-13 21:56:45 +0800

    feat(context): add AuthContext for global auth state management

---
+++ client/src/context/authContext.js
@@ -0,0 +1,41 @@
+import React, { createContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (userData, tokenData) => {
+    setToken(tokenData);
+    setUser(userData);
+
+    localStorage.setItem("token", tokenData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
