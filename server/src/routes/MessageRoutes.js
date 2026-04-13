fc9542fe2a916742d396ff1cd8cdd43f9342809a
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-13 13:38:33 +0800

    feat(routes): add protectRoute middleware to message routes

--- server/src/routes/MessageRoutes.js
+++ server/src/routes/MessageRoutes.js
@@ -3,12 +3,13 @@ import createConversationController from "../controllers/message/createConversat
 import sendMessageController from "../controllers/message/sendMessageController.js";
 import getUserConversationsController from "../controllers/message/getUserConversationsController.js";
 import getMessageController from "../controllers/message/getMessagesController.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/:id/conversation", createConversationController);
-router.post("/:id", sendMessageController);
-router.get("/conversations", getUserConversationsController);
-router.get("/:id", getMessageController);
+router.post("/:id/conversation", protectRoute, createConversationController);
+router.post("/:id", protectRoute, sendMessageController);
+router.get("/conversations", protectRoute, getUserConversationsController);
+router.get("/:id", protectRoute, getMessageController);
 
 export default router;
