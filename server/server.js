e9fe38731e43aad584894b47346d72b54159ccfd
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-13 17:47:07 +0800

    feat(sockets): verify socket.io real time messaging working

--- server/server.js
+++ server/server.js
@@ -29,3 +29,27 @@ connectMONGODB()
     console.log(error);
     process.exit(1);
   });
+
+/*
+-- SERVER TESTING -- 
+
+TERMMINAL : 
+
+Connected to MONGODB successfully
+Server running on http://localhost:8080
+a user connected NjxwuIMy16DPCgF7AAAB
+a user joined room : 69dc7d1a78918a27e46fae95
+user disconnected: NjxwuIMy16DPCgF7AAAB
+a user connected X3GMiU3Qsq1TgvamAAAD
+user disconnected: NjxwuIMy16DPCgF7AAAB
+a user connected X3GMiU3Qsq1TgvamAAAD
+a user connected X3GMiU3Qsq1TgvamAAAD
+a user joined room : 69dc7d1a78918a27e46fae95 
+
+CONSOLE: 
+
+{conversationId: '69dc7d1a78918a27e46fae95', 
+sender: '69dc76822e6b8e78053a5a21', 
+text: 'hello from socket', 
+isRead: false, _id: '69dcba31a637deea186b3ba6', …}
+*/
