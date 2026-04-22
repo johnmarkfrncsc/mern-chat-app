93853524933100c22659c05cf98e61ed69aaedd2
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-22 18:51:26 +0800

    feat(app): mount settings routes in express app

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import AuthRoutes from "./src/routes/AuthRoutes.js";
 import MessageRoutes from "./src/routes/MessageRoutes.js";
 import SearchUserRoutes from "./src/routes/SearchUserRoutes.js";
+import SettingsRoutes from "./src/routes/SettingsRoute.js";
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use("/api/auth", AuthRoutes);
 app.use("/api/message", MessageRoutes);
 app.use("/api/search", SearchUserRoutes);
+app.use("/api/settings", SettingsRoutes);
 
 export default app;
