1de63dddd290dc546e9401f57e32bf91c14ccc5c
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-22 18:05:47 +0800

    feat(controllers): add changeUsernameController

---
+++ server/src/controllers/user/ChangeUsernameController.js
@@ -0,0 +1,21 @@
+import changeUsername from "../../services/user/ChangeUsernameService.js";
+
+const changeUsernameController = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const newUsername = req.body.username;
+
+    const newUser = await changeUsername(userId, newUsername);
+    res.status(200).json({
+      data: newUser,
+      message: "Username changed successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      data: null,
+      message: error.message,
+    });
+  }
+};
+
+export default changeUsernameController;
