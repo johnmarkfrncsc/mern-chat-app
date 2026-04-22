875c547d14f6c126cfc89bf118c570036f05c4bc
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-22 18:21:10 +0800

    feat(controller): add unauthorized check if userId is missing

--- server/src/controllers/user/ChangeUsernameController.js
+++ server/src/controllers/user/ChangeUsernameController.js
@@ -3,6 +3,11 @@ import changeUsername from "../../services/user/ChangeUsernameService.js";
 const changeUsernameController = async (req, res) => {
   try {
     const userId = req.user.id;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
     const newUsername = req.body.username;
 
     const newUser = await changeUsername(userId, newUsername);
