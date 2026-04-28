11f6a325151d437a50b44230ff7813c33fb137a0
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-29 01:09:54 +0800

    feat(conversation): emit newConversation event to receiver socket

--- server/src/controllers/message/createConversationController.js
+++ server/src/controllers/message/createConversationController.js
@@ -1,10 +1,21 @@
+import { getIO, userSocketMap } from "../../config/socket.js";
 import createConversation from "../../services/message/createConversation.js";
 
 const createConversationController = async (req, res) => {
   try {
     const senderId = req.user.id;
     const receiverId = req.params.id;
+
+    const io = getIO();
+
     const newConversation = await createConversation(senderId, receiverId);
+
+    const receiverSocketId = userSocketMap[receiverId];
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newConversation", newConversation);
+    }
+
     return res.status(201).json({
       data: newConversation,
       message: "Conversation created successfully",
