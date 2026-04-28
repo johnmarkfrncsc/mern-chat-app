8cdacfc31dee404916ec2f879147ccb82c942dff
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-29 01:10:23 +0800

    feat(socket): implement user connection handling and online user tracking

--- server/src/config/socket.js
+++ server/src/config/socket.js
@@ -2,12 +2,35 @@ import { Server } from "socket.io";
 
 let io;
 
+export const userSocketMap = {};
+
 const createSocketServer = (server) => {
   io = new Server(server, {
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
     },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("addUser", (userId) => {
+      userSocketMap[userId] = socket.id;
+
+      io.emit("onlineUsers", Object.keys(userSocketMap));
+    });
+
+    socket.on("disconnect", () => {
+      for (const [userId, socketId] of Object.entries(userSocketMap)) {
+        if (socketId === socket.id) {
+          delete userSocketMap[userId];
+          break;
+        }
+      }
+
+      io.emit("onlineUsers", Object.keys(userSocketMap));
+    });
   });
 
   return io;
