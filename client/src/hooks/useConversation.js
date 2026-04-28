2d6409d24acc5ff0baccec62dc6ccb27f56afb1c
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-29 01:10:44 +0800

    feat(conversation): integrate socket for real-time new conversation updates and prevent duplicates

--- client/src/hooks/useConversation.js
+++ client/src/hooks/useConversation.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { getUserConversations, createConversation } from "../api/chat.js";
 import searchUsers from "../api/user.js";
+import useChat from "./useChat.js";
 
 const useConversation = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -8,33 +9,54 @@ const useConversation = () => {
   const [conversations, setConversations] = useState([]);
   const containerRef = useRef(null);
 
+  const { socket } = useChat();
+
   // load convo list
   useEffect(() => {
     const load = async () => {
       const res = await getUserConversations();
-
       setConversations(res.data);
     };
 
     load();
   }, []);
 
-  //debouncing
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleNewConversation = (conversation) => {
+      setConversations((prev) => {
+        // prevent duplicate
+        const exists = prev.find((c) => c._id === conversation._id);
+        if (exists) return prev;
+
+        return [conversation, ...prev];
+      });
+    };
+
+    socket.on("newConversation", handleNewConversation);
+
+    return () => {
+      socket.off("newConversation", handleNewConversation);
+    };
+  }, [socket]);
+
+  // debouncing search
   useEffect(() => {
     if (!searchQuery) {
       setSearchResult([]);
       return;
     }
+
     const timer = setTimeout(async () => {
       const result = await searchUsers(searchQuery);
       setSearchResult(result.data);
     }, 500);
-    return () => {
-      clearTimeout(timer);
-    };
+
+    return () => clearTimeout(timer);
   }, [searchQuery]);
 
-  //ref for clickoutside
+  // click outside
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (containerRef.current && !containerRef.current.contains(e.target)) {
@@ -51,8 +73,10 @@ const useConversation = () => {
 
   const handleSelectUser = async (userId) => {
     await createConversation(userId);
+
     const res = await getUserConversations();
     setConversations(res.data);
+
     setSearchQuery("");
     setSearchResult([]);
   };
