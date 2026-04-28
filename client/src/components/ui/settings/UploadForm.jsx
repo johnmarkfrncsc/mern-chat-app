c970101f95c7af7d4aa7f5a6dab1982095940274
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-29 00:01:56 +0800

    feat(settings): add image preview with remove/reset option for profile upload

--- client/src/components/ui/settings/UploadForm.jsx
+++ client/src/components/ui/settings/UploadForm.jsx
@@ -1,6 +1,6 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { uploadPhoto } from "../../../api/settings.js";
-import { ImageUp } from "lucide-react";
+import { ImageUp, X } from "lucide-react";
 
 const UploadForm = ({
   formData,
@@ -9,7 +9,29 @@ const UploadForm = ({
   loading,
   message,
 }) => {
+  const [preview, setPreview] = useState(null);
   const fileInputRef = useRef(null);
+
+  useEffect(() => {
+    if (!formData.photo) {
+      setPreview(null);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(formData.photo);
+    setPreview(objectUrl);
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [formData.photo]);
+
+  const handleRemove = () => {
+    updateField("photo", null);
+    setPreview(null);
+
+    if (fileInputRef.current) {
+      fileInputRef.current.value = null;
+    }
+  };
 
   return (
     <form
@@ -23,25 +45,43 @@ const UploadForm = ({
       }}
       className="flex flex-col gap-4"
     >
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-2">
         <label className="text-xs font-bold text-gray-500 uppercase">
           Profile Photo
         </label>
 
-        {/* Hidden input */}
+        {/* hidden input */}
         <input
           ref={fileInputRef}
           type="file"
+          accept="image/*"
           onChange={(e) => updateField("photo", e.target.files[0])}
           className="hidden"
+          id="photo-upload"
         />
 
-        {/* Clickable icon */}
-        <ImageUp
-          size={28}
-          className="text-gray-400 cursor-pointer hover:text-teal-600 transition"
-          onClick={() => fileInputRef.current?.click()}
-        />
+        {preview ? (
+          <div className="relative w-fit">
+            <img
+              src={preview}
+              alt="Preview"
+              className="w-28 h-28 rounded-full object-cover border"
+            />
+
+            {/* remove button */}
+            <button
+              type="button"
+              onClick={handleRemove}
+              className="absolute -top-2 -right-4 cursor-pointer"
+            >
+              <X size={20} className="text-gray-500 hover:text-red-500" />
+            </button>
+          </div>
+        ) : (
+          <label htmlFor="photo-upload" className="cursor-pointer w-fit">
+            <ImageUp className="text-gray-500 hover:text-teal-600 transition" />
+          </label>
+        )}
       </div>
 
       <button
