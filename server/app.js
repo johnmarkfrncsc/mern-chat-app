56a8434e2e3ed5486fa7d053584c65bbe002b3f2
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-29 14:35:11 +0800

    feat(routes): add UserProfileRoutes for user profile management and rename SettingsRoutes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import AuthRoutes from "./src/routes/AuthRoutes.js";
 import MessageRoutes from "./src/routes/MessageRoutes.js";
 import SearchUserRoutes from "./src/routes/SearchUserRoutes.js";
-import SettingsRoutes from "./src/routes/SettingsRoute.js";
+import SettingsRoutes from "./src/routes/SettingsRoutes.js";
+import UserProfileRoutes from "./src/routes/UserProfileRoute.js";
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use("/api/auth", AuthRoutes);
 app.use("/api/message", MessageRoutes);
 app.use("/api/search", SearchUserRoutes);
 app.use("/api/settings", SettingsRoutes);
+app.use("/api/profile", UserProfileRoutes);
 
 export default app;
