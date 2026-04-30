02889b4c2e0bf62f37617592409ca84e206b0d42
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-30 18:13:50 +0800

    feat(sendMessageService): enhance sendMessage function to emit conversation updates to participants

--- server/src/services/Message/sendMessage.js
+++ server/src/services/Message/sendMessage.js
@@ -1,5 +1,6 @@
 import ConversationModel from "../../models/ConversationModel.js";
 import MessageModel from "../../models/MessageModel.js";
+import { getIO, userSocketMap } from "../../config/socket.js";
 
 const sendMessage = async (conversationId, senderId, text) => {
   try {
@@ -8,13 +9,26 @@ const sendMessage = async (conversationId, senderId, text) => {
       sender: senderId,
       text,
     });
-    await ConversationModel.findByIdAndUpdate(conversationId, {
-      $set: { lastMessage: newMessage._id },
-    });
+
+    const updatedConversation = await ConversationModel.findByIdAndUpdate(
+      conversationId,
+      { $set: { lastMessage: newMessage._id } },
+      { new: true },
+    )
+      .populate("participants", "username profilePhoto lastSeen")
+      .populate("lastMessage", "text");
 
     const populatedMessage = await MessageModel.findById(
       newMessage._id,
     ).populate("sender", "username");
+
+    const io = getIO();
+    updatedConversation.participants.forEach((participant) => {
+      const socketId = userSocketMap[participant._id.toString()];
+      if (socketId) {
+        io.to(socketId).emit("conversationUpdated", updatedConversation);
+      }
+    });
 
     return populatedMessage;
   } catch (error) {
